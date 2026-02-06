c6590c238fe1c57963034b05c9016ae0f430e263
JDParay <jdparay@mcm.edu.ph>
2026-02-06 12:14:40 +0800

    pause stuff

--- Assets/Scripts/PauseScreen.cs
+++ Assets/Scripts/PauseScreen.cs
@@ -1,61 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
 
 public class PauseScreen : MonoBehaviour
 {
     public PlayerMovement player;
-    public Animator pauseScreenAnimator;
-    public float transitionTime = 1f;
+    public CanvasGroup pauseGroup;
+
     private bool windowActive = false;
     private bool gamePause = false;
-    // public readonly int isActiveHash = Animator.StringToHash("isActive");
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        SetPauseUI(false); // start hidden
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (windowActive == false)
-            {
-                player.SetInputPlayerStatus(false);
-                windowActive = true;
-                gamePause = true;
-                StartCoroutine(PauseFade("FadeIn"));
-            } else
-            {
-                player.SetInputPlayerStatus(true);
-                windowActive = false;
-                gamePause = false;
-                StartCoroutine(PauseFade("FadeOut"));
-            }
+            windowActive = !windowActive;
+            gamePause = windowActive;
+
+            player.SetInputPlayerStatus(!windowActive);
+            PauseGame();
+            SetPauseUI(windowActive);
         }
     }
 
     void PauseGame()
     {
-        if (gamePause == true)
-        {
-            Time.timeScale = 0f;
-        } else if (gamePause == false)
-        {
-            Time.timeScale = 1f;
-        }
+        Time.timeScale = gamePause ? 0f : 1f;
     }
 
-    IEnumerator PauseFade(string triggerName)
+    void SetPauseUI(bool show)
     {
-        if (pauseScreenAnimator != null)
-        {
-            pauseScreenAnimator.SetTrigger(triggerName);
-        }
-        yield return new WaitForSecondsRealtime(transitionTime);
-        PauseGame();
+        pauseGroup.alpha = show ? 1f : 0f;
+        pauseGroup.interactable = show;
+        pauseGroup.blocksRaycasts = show;
     }
 }
